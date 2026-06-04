2c4d7c29c4019df36a7d59f9b8a2e1d026b5d448
onehoon <onehoon@gmail.com>
2026-06-04 21:52:40 +0900

    Reduce main view model section proxies

--- OptiClick.Wpf/ViewModels/MainViewModel.SupportedGamesWiki.cs
+++ OptiClick.Wpf/ViewModels/MainViewModel.SupportedGamesWiki.cs
@@ -6,38 +6,6 @@ namespace OptiClick.Wpf.ViewModels;
 
 public sealed partial class MainViewModel : ViewModelBase
 {
-    private void EnsureSupportedGamesWikiLoadedForView()
-    {
-        SupportedGames.EnsureLoadedForView();
-    }
-
-    private void StartSupportedGamesWikiRefreshInBackground()
-    {
-        SupportedGames.StartRefreshInBackground();
-    }
-
-    private void RefreshSupportedGamesAfterLanguageChange()
-    {
-        SupportedGames.RefreshAfterLanguageChange();
-    }
-
-    private void RebuildSupportedGamesWikiRows()
-    {
-        SupportedGames.RebuildRows();
-    }
-
-    public void QueueSupportedGamesWikiVisibleCoverLoad(double verticalOffset, double viewportHeight)
-    {
-        SupportedGames.QueueVisibleCoverLoad(verticalOffset, viewportHeight);
-    }
-
-    private IReadOnlyList<SupportedGamesWikiRowViewModel> ResolveSupportedGamesWikiVisibleRows(
-        double verticalOffset,
-        double viewportHeight)
-    {
-        return SupportedGames.ResolveVisibleRows(verticalOffset, viewportHeight);
-    }
-
     internal static string ResolveSupportedGamesWikiCoverSource(
         SupportedGamesWikiEntry entry,
         string defaultCoverSource = CoverImageCacheService.DefaultCoverImageSource)
