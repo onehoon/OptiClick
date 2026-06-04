a202f6c948bcd00dadbcf1185eff5a61ca049ec9
onehoon <onehoon@gmail.com>
2026-06-04 21:02:03 +0900

    Remove shell command proxies

--- OptiClick.Wpf/ViewModels/MainViewModel.Commands.cs
+++ OptiClick.Wpf/ViewModels/MainViewModel.Commands.cs
@@ -1,4 +1,3 @@
-using System.Windows.Input;
 using OptiClick.Wpf.ViewModels.Shell;
 
 namespace OptiClick.Wpf.ViewModels;
@@ -6,22 +5,6 @@ namespace OptiClick.Wpf.ViewModels;
 public sealed partial class MainViewModel
 {
     private RelayCommand _openGameSupportRequestCommand = null!;
-
-    public ICommand SelectGameCommand => Home.SelectGameCommand;
-    public ICommand ShowHomeCommand => Commands.ShowHomeCommand;
-    public ICommand ShowSupportedGamesWikiViewCommand => Commands.ShowSupportedGamesWikiViewCommand;
-    public ICommand ShowScanViewCommand => Commands.ShowScanViewCommand;
-    public ICommand ShowSettingsCommand => Commands.ShowSettingsCommand;
-    public ICommand AddScanFolderCommand => Scan.AddScanFolderCommand;
-    public ICommand RemoveScanFolderCommand => Scan.RemoveScanFolderCommand;
-    public ICommand OpenScanFolderCommand => Scan.OpenScanFolderCommand;
-    public ICommand SaveAndScanCommand => Scan.SaveAndScanCommand;
-    public ICommand OpenLogFolderCommand => Settings.OpenLogFolderCommand;
-    public ICommand OpenSupportRequestCommand => Settings.OpenSupportRequestCommand;
-    public ICommand OpenGameSupportRequestCommand => _openGameSupportRequestCommand;
-    public ICommand RefreshInstallFilesCommand => Settings.RefreshInstallFilesCommand;
-    public ICommand ShowDetailsCommand => Home.ShowDetailsCommand;
-    public ICommand ShowInstallCommand => Home.ShowInstallCommand;
 
     private void InitializeCommandSet()
     {
